8c2f4dd14e711e78c2e23ea985469c4357bd5983
Starwolf16 <jmtarrant00@gmail.com>
2022-09-15 23:44:24 -0400

    Begin next space function

--- Assignment 1 No GUI.py
+++ Assignment 1 No GUI.py
@@ -4,6 +4,7 @@
 # Due Date:     09/16/2022
 
 #imports
+from cgitb import small
 import math as math
 from random import randint
 import numpy as np
@@ -23,17 +24,31 @@ board = [[0 for y in range(h)] for x in range(w)]
 board[targetX][targetY] = 2 # Set target to 2
 board[searchX][searchY] = 1 # Set search to 1
 
-# Distance between Function
+# Euclidean Distance Function
 def distance(targetX, targetY, searchX, searchY):
     return math.sqrt(pow((targetX-searchX), 2) + pow((targetY - searchY), 2))
 
-
-
-# run = True
-# while run:
-#     if searchX == 0 or searchX == 4:
-#         if searchY == 0 or searchY == 4:
-#             print 
+# Find next space
+def next(nSearchX, nSearchY):
+    if nSearchX == targetX and nSearchY == targetY:
+        return 0
+        
+    distances = list()
+    for x in range(4):
+        if x == 0 and nSearchX != 0:
+            distances.append((distance(targetX, targetY, nSearchX-1, nSearchY), nSearchX-1, nSearchY))
+        elif x == 1 and nSearchY != 0:
+            distances.append((distance(targetX, targetY, nSearchX, nSearchY-1), nSearchX, nSearchY-1))
+        elif x == 2 and nSearchX != 4:
+            distances.append((distance(targetX, targetY, nSearchX+1, nSearchY), nSearchX+1, nSearchY))
+        elif x == 3 and nSearchY != 4:
+            distances.append((distance(targetX, targetY, nSearchX, nSearchY+1), nSearchX, nSearchY+1))
+    smallest = distances[0]
+    for x in distances:
+        if distances[x+1][0] < smallest[0]:
+            smallest = distances[x+1]
+    next(smallest[1], smallest[2])
+        
 
 
 print("Board: ")
